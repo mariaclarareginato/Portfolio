08f8038dadf705073deafdd01675352a2956c91c
Maria Clara Reginato <mariaclarareginato8@gmail.com>
2025-12-16 20:22:34 -0300

    Atualização NEXT, idioma e icon

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({children }) {
 
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>
           <ThemeProvider
           attribute="class"
